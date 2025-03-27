e3754c01c103021515c3d704df8a9fe10fcfbddc
Deviicho <Devviicho@gmail.com>
2025-03-27 17:16:38 +0300

    Delete auth_page directory

--- auth_page/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('' , views.signuppage , name='signup'),
-    path('login/' , views.loginpage , name='login'),
-    path('logout/' , views.logoutUser , name='logout'),
-]
-
